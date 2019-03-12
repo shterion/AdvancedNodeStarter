1b35c52042d72595b8c631daec2a2c6eb057c0cf
Shterion Yanev <shterion.yanev@yahoo.com>
2019-03-12 23:28:01 +0100

    Expand helper with login and getContentsOf functions

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("./../factories/sessionFactory");
+const userFactory = require("./../factories/userFactory");
 
 class CustomPage {
   static async build() {
@@ -6,17 +8,37 @@ class CustomPage {
       headless: false
     });
 
-    const page = browser.newPage();
+    const page = await browser.newPage();
     const customPage = new CustomPage(page);
     return new Proxy(customPage, {
       get: function(target, property) {
-        return target[property] || page[property] | browser[property];
+        return target[property] || browser[property] || page[property];
       }
     });
   }
 
   constructor(page) {
     this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const {session, sig} = sessionFactory(user);
+
+    await this.page.setCookie({
+      name: "session",
+      value: session
+    });
+    await this.page.setCookie({
+      name: "session.sig",
+      value: sig
+    });
+    await this.page.goto("localhost:3000/blogs");
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  getContentsOf(selector) {
+    return this.page.$eval(selector, el => el.innerHTML);
   }
 }
 
