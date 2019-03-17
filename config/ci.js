d60d864b5b6ccea86b0a0e3c3a12785817f89f9f
Shterion Yanev <shterion.yanev@yahoo.com>
2019-03-17 09:18:36 +0100

    Setup env vars direct in travis

--- config/ci.js
+++ config/ci.js
@@ -1,9 +1,7 @@
 module.exports = {
-  googleClientID:
-    // "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
-    "762771318554-1aft5avf8dsocnercur8kt6ggle757eu.apps.googleusercontent.com",
-  googleClientSecret: "sik_5p0fzIHnLQ-M4T1nrZTI", //"KnH-rZC23z4fr2CN4ISK4srN",
-  mongoURI: "mongodb://localhost:27017/blog_ci",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379"
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  redisUrl: process.env.REDIS_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
